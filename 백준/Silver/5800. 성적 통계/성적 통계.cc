3c9ed15860e8f38a44d7304a57b4dd7510d50eaf
yeontachi <162268704+yeontachi@users.noreply.github.com>
2024-03-05 12:37:15 +0900

    [Silver V] Title: 성적 통계, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/5800. 성적 통계/성적 통계.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int K; cin>>K;
+    int arr[51];
+
+    for(int k = 1; k<=K; k++){ //K번 반복
+        int N; cin >> N;
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }      
+
+        sort(arr, arr+N, greater<int>()); //내림차순 정렬
+        
+        cout << "Class " << k << endl;
+        cout << "Max " << arr[0] << ", ";
+        cout << "Min " << arr[N - 1] << ", ";     
+
+        int gap, largest = 0; //gap: 인접한 점수 차, largest: gap의 최댓값
+        for(int i = 1; i < N; i++){
+            gap = arr[i - 1] - arr[i];
+            if(largest < gap)
+                largest = gap;
+        }
+        cout << "Largest gap " << largest << endl;
+    }
+
+    return 0;
+}
