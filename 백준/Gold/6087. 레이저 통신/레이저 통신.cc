97177766c0e42cc9bad064e885542175147e217e
yeontachi <162268704+yeontachi@users.noreply.github.com>
2024-04-02 12:36:33 +0900

    [Gold III] Title: 레이저 통신, Time: 0 ms, Memory: 2080 KB -BaekjoonHub

--- 백준/Gold/6087. 레이저 통신/레이저 통신.cc
+++ 백준/Gold/6087. 레이저 통신/레이저 통신.cc
@@ -1,56 +1,52 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 using namespace std;
 
-char v[101][101];
-vector<pair<int,int>>vv;
-int mirror[101][101];
-queue<pair<pair<int,int>,pair<int,int>>>q;
-int main() {
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    int W,H;
-    cin>>W>>H;
-    for(int i=0; i<H; i++) {
-        for(int j=0; j<W; j++) {
-            cin>>v[i][j];
-            if(v[i][j]=='C') vv.push_back({i,j});
-            mirror[i][j]=-1;
+int dx[4] = {0, 1, 0, -1};
+int dy[4] = {1, 0, -1, 0};
+char arr[100][100];
+int cnt[100][100] = {0, };
+bool visit[100][100] = {0, };
+int W, H;
+
+void bfs(int x, int y){
+  queue< pair<int, int> > bfsQ;
+  bfsQ.push(pair<int, int>(x, y));
+
+  while(!bfsQ.empty()){
+    pair<int, int> p = bfsQ.front();
+    bfsQ.pop();
+    for (int i = 0; i < 4; i++) {
+      int nx = p.first + dx[i];
+      int ny = p.second + dy[i];
+      while(nx >= 0 && ny >= 0 && nx < H && ny < W && arr[nx][ny] != '*'){
+        if(cnt[nx][ny] == 0){
+          cnt[nx][ny] = cnt[p.first][p.second] + 1;
+          bfsQ.push(pair<int, int>(nx, ny));
         }
+        nx += dx[i];
+        ny += dy[i];
+      }
     }
-    int dx[4]={-1,0,0,1};
-    int dy[4]={0,-1,1,0};
-    q.push({{vv[0].first, vv[0].second},{-1,-1}});
-    while(!q.empty()) {
-        int qx = q.front().first.first;
-        int qy = q.front().first.second;
-        int dir = q.front().second.first;
-        int cnt = q.front().second.second;
-        for(int i=0; i<4; i++) {
-            int x=qx+dx[i];
-            int y=qy+dy[i];
-            if(x>=0 && x<H && y>=0 && y<W) {
-                if(v[x][y]=='.') {
-                    if(mirror[x][y]==-1 || mirror[x][y]>cnt) {
-                        if(dir!=i) mirror[x][y]=cnt+1;
-                        else mirror[x][y]=cnt;
-                        q.push({{x,y},{i,mirror[x][y]}});
-                    }
-                }
-                else if(x==vv[1].first && y==vv[1].second) {
-                    if(mirror[x][y]==-1) {
-                        if(dir!=i) mirror[x][y]=cnt+1;
-                        else mirror[x][y]=cnt;
-                    }
-                    else {
-                        if(dir!=i) mirror[x][y]=min(mirror[x][y], cnt+1);
-                        else mirror[x][y]=min(mirror[x][y], cnt);
-                    }
-                }
-            }
-        }
-        q.pop();
+  }
+}
+
+int main(){
+  cin >> W >> H;
+  int x, y;
+  int endX, endY;
+  bool chk = false;
+  for (int i = 0; i < H; i++) {
+    for (int j = 0; j < W; j++) {
+      cin >> arr[i][j];
+      if(arr[i][j] == 'C' && !chk) { x=i; y=j; chk=true; }
+      else if(arr[i][j] == 'C' && chk) { endX=i; endY=j; }
     }
-    cout<<mirror[vv[1].first][vv[1].second];
+  }
+
+  bfs(x,y);
+
+  cout << cnt[endX][endY]-1 << endl;
+
+  return 0;
 }
